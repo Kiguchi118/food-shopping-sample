a162a9b057b59315be604bd6cfd93ebc27d517cf
KIguchi <kiguchi118@gmail.com>
2020-03-24 04:46:01 +0900

    feature user login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,12 @@ class ApplicationController < ActionController::Base
 
     protected
     def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:email,:password,:password_confirmation,:last_name,:first_name,
+                                                           :last_name_kana,:first_name_kana,:postcode,:prefecture_name,:address_city,
+                                                           :address_street,:address_building,:telephone_number])
         devise_parameter_sanitizer.permit(:sign_in, keys: [:email,:password])
+        devise_parameter_sanitizer.permit(:account_update, keys: [:name,:email,:password,:password_confirmation,:last_name,:first_name,
+                                                           :last_name_kana,:first_name_kana,:postcode,:prefecture_name,:address_city,
+                                                           :address_street,:address_building,:telephone_number])
     end
 end
