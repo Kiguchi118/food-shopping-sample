c53f588e6f6e934ebe88f32ee5a069d2a4f6c491
KIguchi <kiguchi118@gmail.com>
2020-03-30 03:52:21 +0900

    Add cart page (index & create action)

--- app/controllers/users/cart_items_controller.rb
+++ app/controllers/users/cart_items_controller.rb
@@ -1,5 +1,34 @@
 class Users::CartItemsController < ApplicationController
   before_action :authenticate_user!
+
   def index
+    @cart_items = CartItem.where(user_id: current_user.id)
+    @total_price = 0
+    @total_amount = 0
   end
+
+  def create
+    @cart_item = CartItem.new(cart_item_params)
+    if @cart_item.save
+      flash[:success] = "カートに商品を入れました"
+      redirect_to cart_items_url
+    else
+      @item = Item.find(params[:id])
+      render "items/show"
+    end
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+    def cart_item_params
+      params.require(:cart_item).permit(:user_id,:item_id,:amount)
+    end
 end
