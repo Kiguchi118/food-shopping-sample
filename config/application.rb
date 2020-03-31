7b5b37aac351a9cc86722954f54a8f1f7ef47306
KIguchi <kiguchi118@gmail.com>
2020-04-01 04:14:13 +0900

    Add Japanese time

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,9 @@ module CakeShopping
 
     config.autoload_paths += Dir[Rails.root.join('app', 'uploaders')]
 
+    # タイムゾーンを日本時間に設定
+    config.time_zone = 'Asia/Tokyo'
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
