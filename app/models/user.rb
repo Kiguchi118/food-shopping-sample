132632f81b41ced29c6de16bf3a6cb33c6a381f3
KIguchi <kiguchi118@gmail.com>
2020-04-04 02:53:46 +0900

    Set user validation & edit Japanese devise...

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :name, presence: true
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :postcode, presence: true
+  validates :prefecture_name, presence: true
+  validates :address_city, presence: true
+  validates :address_street, presence: true
+  validates :address_building, presence: true
+  validates :telephone_number, presence: true
 
   has_many :cart_items, dependent: :destroy
   has_many :orders, dependent: :destroy
