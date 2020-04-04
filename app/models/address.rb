f86786a3d71ff4acf10ea2db705d38263ad741fe
KIguchi <kiguchi118@gmail.com>
2020-04-05 02:49:36 +0900

    Add order payment_method validation & fix order settings

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,8 @@
 class Address < ApplicationRecord
   belongs_to :user
+
+  def view_address
+    '〒' + self.postcode.to_s + self.address_city + self.address_street + self.address_building + self.name
+  end
+
 end
