856d8ccaae2fdb4da4f0db87e10febb266a7c6f8
KIguchi <kiguchi118@gmail.com>
2020-04-10 00:17:28 +0900

    [Add]住所編集，削除機能 & [Fix]addressesコントローラ

--- app/controllers/users/addresses_controller.rb
+++ app/controllers/users/addresses_controller.rb
@@ -1,16 +1,18 @@
 class Users::AddressesController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_addresses, only: [:index]
+  before_action :set_addresses_index, only: [:index]
+  before_action :set_address, only: [:edit, :update]
 
   def index
   end
 
   def create
-    address = current_user.addresses.build(address_params)
-    if address.save
+    @address = current_user.addresses.build(address_params)
+    @address.name = current_user.first_name
+    if @address.save
       redirect_to addresses_url, flash: { success: "住所を登録できました！" }
     else
-      set_addresses
+      set_addresses_index
       render "index"
     end
   end
@@ -19,19 +21,28 @@ class Users::AddressesController < ApplicationController
   end
 
   def update
-
+    if @address.update(address_params)
+      redirect_to addresses_url, flash: { success: "住所を編集しました！"}
+    else
+      render "edit"
+    end
   end
 
   def destroy
-
+    Address.find(params[:id]).destroy
+    redirect_to addresses_url, flash: { success: "登録していた住所を削除しました" }
   end
 
   private
 
-    def set_addresses
+    def set_addresses_index
       @new_address = Address.new
       @addresses = Address.where(user_id: current_user.id)
       @user = current_user
+    end
+
+    def set_address
+      @address = Address.find(params[:id])
     end
 
     def address_params
