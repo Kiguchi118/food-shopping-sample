97415ec0f14b38d2a46a94b797f85b00dd46ed97
KIguchi <kiguchi118@gmail.com>
2020-04-23 02:48:33 +0900

    [Fix] コメント機能修正(非同期通信)

--- app/controllers/users/comments_controller.rb
+++ app/controllers/users/comments_controller.rb
@@ -1,15 +1,13 @@
 class Users::CommentsController < ApplicationController
 
   def create
-    comment = current_user.comments.build(comment_params)
-    if comment.save
+    @comment = current_user.comments.build(comment_params)
+    if @comment.save
       flash.now[:success] = "コメントを投稿しました！"
+      @comments = @comment.item.comments.order(created_at: :desc)
     else
       flash.now[:denger] = "コメントが空欄です！"
     end
-    @item = Item.find(comment.item_id)
-    @comment = Comment.new  # 連続投稿を可能にさせる
-    @comments = @item.comments.order(created_at: :desc)
   end
 
   def edit
@@ -19,12 +17,10 @@ class Users::CommentsController < ApplicationController
   end
 
   def destroy
-    comment = Comment.find_by(user_id: current_user.id, id: params[:id])
-    @item = Item.find(comment.item_id)
-    comment.destroy
+    @comment = Comment.find_by(user_id: current_user.id, id: params[:id])
+    @comments = @comment.item.comments.order(created_at: :desc)
+    @comment.destroy
     flash.now[:success] = "コメントを削除しました！"
-    @comment = Comment.new
-    @comments = @item.comments.order(created_at: :desc)
   end
 
   private
